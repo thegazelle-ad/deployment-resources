f100c60d6ac6218e48b03160b8ab966a212ff21a
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2018-01-22 10:24:40 +0400

    Split caddy file into staging and production

--- scripts/run_caddy.sh
+++ scripts/run_caddy.sh
@@ -3,4 +3,4 @@
 # Increase maximum amount of file descriptors available, recommended for production
 ulimit -n 8192
 # Run the server
-sudo ./caddy/caddy -conf ./config/Caddyfile
+sudo ./caddy/caddy -conf "./config/$GAZELLE_ENV.caddy"
